e2ce81392fa79b0d069fcd08829a35b4b2084f49
jbenjaminy <young.benjamin@gmail.com>
2016-07-19 14:59:32 -0500

    Adds stateful conponent for person

--- js/index.jsx
+++ js/index.jsx
@@ -2,16 +2,30 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 // COMPONENT
-var Person = function(props) {
-    // Returns React element
-    return (
-        <div className="person">
-            <div className="person-name">{props.name}</div>
-            <img className="person-img" src={props.imageUrl}/>
-            <div className="person-job">{props.job}</div>
-        </div>
-    );
-};
+var Person = React.createClass({
+    getInitialState: function() {
+        return {
+            highlight: false
+        };
+    },
+    onClick: function() {
+        this.setState({
+            highlight: !this.state.highlight
+        });
+    },
+    render: function() {
+        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+        return (
+            <div className={classes} onClick={this.onClick}>
+                <div className="person-name">{this.props.name}</div>
+                <img className="person-img" src={this.props.imageUrl} />
+                <div className="person-job">
+                    {this.props.job}
+                </div>
+            </div>
+        );
+    }
+});
 
 Person.defaultProps = {
     imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
